5db98db5a654b3ef392b35bc5eed236e6461daea
JosephFrakes <jofrakes@iu.edu>
2022-04-22 15:20:46 -0400

    Client Sessions Created

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var dataServ = require("./data-service.js");
 const res = require("express/lib/response");
 const exphbs = require ("express-handlebars");
 const mongoose = require ("mongoose");
+const clientSessions = require("client-sessions");
 
 const app = express();
 
@@ -29,8 +30,79 @@ function onHttpStart() {
   console.log("Express http server listening on Port: " + HTTP_PORT);
 };
 
+// Setup client-sessions
+app.use(clientSessions({
+  cookieName: "session", // this is the object name that will be added to 'req'
+  secret: "rims05_hairstyling101", // this should be a long un-guessable string.
+  duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+  activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
+}));
+
+// Parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
 
 
+// A simple user object, hardcoded for this example
+const user = {
+  username: "sampleuser",
+  password: "samplepassword",
+};
+
+// Setup a route on the 'root' of the url to redirect to /login
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+// Display the login html page
+app.get("/login", function(req, res) {
+  res.render("login", { layout: false });
+});
+
+// The login route that adds the user to the session
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(username === "" || password === "") {
+    // Render 'missing credentials'
+    return res.render("login", { errorMsg: "Missing credentials.", layout: false });
+  }
+
+  // use sample "user" (declared above)
+  if(username === user.username && password === user.password){
+
+    // Add the user on the session and redirect them to the dashboard page.
+    req.session.user = {
+      username: user.username,
+      email: user.email
+    };
+
+    res.redirect("/home");
+  } else {
+    // render 'invalid username or password'
+    res.render("login", { errorMsg: "invalid username or password!", layout: false});
+  }
+});
+
+// Log a user out by destroying their session
+// and redirecting them to /login
+app.get("/logout", function(req, res) {
+  req.session.reset();
+  res.redirect("/login");
+});
+
+
+// This is a helper middleware function that checks if a user is logged in
+// we can use it in any route that we want to protect against unauthenticated access.
+// A more advanced version of this would include checks for authorization as well after
+// checking if the user is authenticated
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
 
 /// setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req, res){
